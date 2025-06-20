93a2adfaf34f1b22f63cdf8c681282b2c15d89ad
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-06-21 03:29:45 +0900

    [Chore] 정보 서머리 추가

--- Assets/02. Scripts/Enum/ELanguage.cs
+++ Assets/02. Scripts/Enum/ELanguage.cs
@@ -1,4 +1,7 @@
 ﻿//EditorCSVEnumWriter.cs에서 자동 생성
+/// <summary>
+/// 언어를 선택할 enum
+/// <summary>
 public enum ELanguage
 {
     EN,
--- Assets/02. Scripts/Enum/ETextInfo.cs
+++ Assets/02. Scripts/Enum/ETextInfo.cs
@@ -1,6 +1,10 @@
 ﻿//EditorCSVEnumWriter에서 자동 생성
+/// <summary>
+/// Text의 정보들을 담을 enum
+/// <summary>
 public enum ETextInfo
 {
+    None,
     Menu_Start,
     Menu_Options,
     Menu_Exit,
