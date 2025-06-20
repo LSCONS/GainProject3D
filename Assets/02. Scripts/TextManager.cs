c690a64e6b1a5eb2b7827c334ab5b026682691e0
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-06-21 03:35:39 +0900

    [Feat] Dictionary 초기화 메서드 및 string 반환 인덱서 추가.

--- Assets/02. Scripts/TextManager.cs
+++ Assets/02. Scripts/TextManager.cs
@@ -1,6 +1,29 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class TextManager : MonoBehaviour
+public class TextManager
 {
+    public Dictionary<ETextInfo, string> DictETextToString = new(); //텍스트 정보를 저장할 Dictionary
 
+    /// <summary>
+    /// 입력받은 enum을 통해 해당하는 string을 반환
+    /// </summary>
+    public string this[ETextInfo eTextInfo]
+    {
+        get
+        {
+            return DictETextToString[eTextInfo];
+        }
+    }
+
+
+    /// <summary>
+    /// Dictionary의 값을 초기화하는 메서드.
+    /// </summary>
+    /// <param name="eLanguage"></param>
+    public void InitDictTextToString(ELanguage eLanguage)
+    {
+        DictETextToString.Clear();
+        DictETextToString = CSVLoader.LoadText(eLanguage);
+    }
 }
